f05b6b2cab841756e4d6db73b4b3a8ee2a09c7d7
Claudivan Barreto <claudvanbarreto@hotmail.com>
2020-01-14 10:42:51 -0300

    feat: add config project

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = { 
+    entry: './index.js', 
+    output: { 
+        path: __dirname + '/dist', 
+        publicPath: '/', 
+        filename: 'bundle.js' 
+    }, 
+    devServer: { 
+        contentBase: './dist', 
+    }, 
+    module: { 
+        rules: [
+            { 
+                test: /\.(js|jsx)$/, 
+                exclude: /node_modules/, 
+                use: ['babel-loader'] 
+            },
+            {
+                test: /\.html$/,
+                use: [
+                  {
+                    loader: "html-loader"
+                  }
+                ]
+            },
+            {    
+                test: /\.less$/, 
+                use: [ 
+                    'style-loader', 
+                    'css-loader',
+                    'less-loader'
+                ] 
+            },
+            {
+                test: /\.(png|jpg|gif)$/i,
+                use: [
+                  {
+                    loader: 'url-loader',
+                  }
+                ]
+            }       
+        ] 
+    }, 
+    plugins: [
+        new HtmlWebPackPlugin({
+          template: "./src/index.html",
+          filename: "./index.html"
+        })
+    ]
+};
