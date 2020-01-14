ca28c78edd340ea29b879e7d1d27aa2e4e6c5a73
Claudivan Barreto <claudvanbarreto@hotmail.com>
2020-01-14 16:50:36 -0300

    feat: add axios

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: './index.js', 
     output: { 
         path: __dirname + '/dist', 
-        publicPath: '/', 
+        publicPath: '', 
         filename: 'bundle.js' 
     }, 
     devServer: { 
