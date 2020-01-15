c97bce3d79b32bf85d138e508f942337abfabd3c
Claudivan Barreto <claudvanbarreto@hotmail.com>
2020-01-14 23:20:28 -0300

    feat: add video modal

--- src/components/Trailer/index.js
+++ src/components/Trailer/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import VideoModal from '../VideoModal';
 
 import './style.less';
 
 export default function Trailer(props){
+    const [showVideo, setShowVideo] = useState(false);
+
     return (
-        <li className="trailer" >
-            <div className="thumbnail" style={{ backgroundImage: `url(${props.thumbnail})` }}></div>
-            <span className="trailer-title">{props.title}</span>
-        </li>
+        <>
+            <li className="trailer" onClick={() => setShowVideo(true)}>
+                <div className="thumbnail" style={{ backgroundImage: `url(${props.thumbnail})` }}></div>
+                <span className="trailer-title">{props.title}</span>
+            </li>
+            { showVideo ? <VideoModal videoId={props.videoId} closeVideo={() => setShowVideo(false)}/> : null}
+        </>
     );
 }
--- src/components/Trailers/index.js
+++ src/components/Trailers/index.js
@@ -9,8 +9,8 @@ export default function Trailers(props){
     return (
         <div className="trailers">
             <ul className="trailers-list">
-                {props.trailers.map(({id, snippet: {title, thumbnails: { maxres }}}) => (
-                    <Trailer key={id} title={title} thumbnail={maxres.url}/>
+                {props.trailers.map(({id, snippet: {title, thumbnails: { maxres }, resourceId}}) => (
+                    <Trailer key={id} title={title} thumbnail={maxres.url} videoId={resourceId.videoId}/>
                 ))}
                 { props.nextPageToken ? 
                     <li className="load-more">
