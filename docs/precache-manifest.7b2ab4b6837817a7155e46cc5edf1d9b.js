6d4ea1fd9310835575751220fd5c53f6de390c1c
Dian Carlos <dian.cabral@gmail.com>
2020-01-31 15:03:41 -0300

    Savepoint

---
+++ docs/precache-manifest.7b2ab4b6837817a7155e46cc5edf1d9b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4efb84ccc9c50f187a680a49af7ccf9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c74c8d41157551703d9",
+    "url": "/static/css/2.3d82f854.chunk.css"
+  },
+  {
+    "revision": "8898677a80e5f2d36992",
+    "url": "/static/css/main.405edf71.chunk.css"
+  },
+  {
+    "revision": "9c74c8d41157551703d9",
+    "url": "/static/js/2.26456582.chunk.js"
+  },
+  {
+    "revision": "8898677a80e5f2d36992",
+    "url": "/static/js/main.b844497f.chunk.js"
+  },
+  {
+    "revision": "fc8fc99120ee0239f9f3",
+    "url": "/static/js/runtime-main.8c36766c.js"
+  },
+  {
+    "revision": "38efc3b1a10a498d42002ed00393f266",
+    "url": "/static/media/background.38efc3b1.png"
+  },
+  {
+    "revision": "2a630ee8b6721efc6c5df127cc3971ca",
+    "url": "/static/media/ingaia.2a630ee8.png"
+  },
+  {
+    "revision": "2c01c1bdadae380f9828c82356a42b21",
+    "url": "/static/media/logo_dark.2c01c1bd.png"
+  },
+  {
+    "revision": "66f90df93590c49adb0b825e898a416a",
+    "url": "/static/media/wild-hunt.66f90df9.png"
+  }
+]);
