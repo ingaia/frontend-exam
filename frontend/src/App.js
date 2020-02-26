5b4929311600f0062b690d585fadc7af5b54f9ec
wesley dias <wesleysd1989@gmail.com>
2020-02-25 21:48:19 -0300

    add load na aplicacao

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { ToastContainer } from 'react-toastify';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
@@ -6,21 +6,29 @@ import { Router } from 'react-router-dom';
 
 import './config/ReactotronConfig';
 
-import Routes from './routes';
 import history from './services/history';
 
 import { store, persistor } from './store';
 
+import { Loader } from '~/components';
 import GlobalStyle from './styles/global';
+
+const Routes = lazy(() => {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(import('./routes')), 3000);
+    });
+});
 
 function App() {
     return (
         <Provider store={store}>
             <PersistGate persistor={persistor}>
                 <Router history={history}>
-                    <Routes />
-                    <GlobalStyle />
-                    <ToastContainer autoClose={3000} />
+                    <Suspense fallback={<Loader />}>
+                        <Routes />
+                        <GlobalStyle />
+                        <ToastContainer autoClose={3000} />
+                    </Suspense>
                 </Router>
             </PersistGate>
         </Provider>
