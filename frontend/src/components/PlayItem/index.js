ce1cacc9ff1020ad6f1858073d907cfb82f8a696
wesley dias <wesleysd1989@gmail.com>
2020-02-25 21:47:49 -0300

    fix play de video no modal

--- frontend/src/components/PlayItem/index.js
+++ frontend/src/components/PlayItem/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import {
@@ -10,25 +10,42 @@ import {
 } from './styles';
 
 export default function PlayItem({ id, thumb, title, videoId }) {
+    const [showIframe, setShowIframe] = useState(false);
+
+    function handlerShowIframe() {
+        setShowIframe(!showIframe);
+    }
     return (
         <>
-            <Container href={`#${id}`} thumb={thumb}>
+            <Container
+                href={`#${id}`}
+                thumb={thumb}
+                onClick={handlerShowIframe}
+            >
                 <div>{title}</div>
             </Container>
-            <Modal id={`${id}`}>
-                <CloseModal href="#fechar" title="Fechar">
-                    CLOSE
-                </CloseModal>
-                <VideoContainer>
-                    <ResponsiveIframe
-                        title={title}
-                        src={`https://www.youtube.com/embed/${videoId}`}
-                        frameBorder="0"
-                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen
-                    />
-                </VideoContainer>
-            </Modal>
+            {console.tron.log()}
+            {showIframe ? (
+                <Modal id={`${id}`}>
+                    <CloseModal
+                        href="#fechar"
+                        title="Fechar"
+                        onClick={handlerShowIframe}
+                    >
+                        CLOSE
+                    </CloseModal>
+
+                    <VideoContainer>
+                        <ResponsiveIframe
+                            title={title}
+                            src={`https://www.youtube.com/embed/${videoId}`}
+                            frameBorder="0"
+                            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen
+                        />
+                    </VideoContainer>
+                </Modal>
+            ) : null}
         </>
     );
 }
