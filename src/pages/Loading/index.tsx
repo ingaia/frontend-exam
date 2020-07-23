415f49446ad8b04e661c0f9f9a90ca7534102168
croschel <croschel000@gmail.com>
2020-07-23 14:11:52 -0300

    Loading Page is ready

--- src/pages/Loading/index.tsx
+++ src/pages/Loading/index.tsx
@@ -1,17 +1,34 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import history from '../../services/history';
 import { Container, Loader } from './styles';
 import loader from '../../assets/loader.svg';
+import loader_1 from '../../assets/loader-1.svg';
+import loader_2 from '../../assets/loader-2.svg';
+import loader_3 from '../../assets/loader-3.svg';
 
 const Loading: React.FC = () => {
+  const [logo, setLogo] = useState(loader);
+
   useEffect(() => {
     setTimeout(() => {
-      history.push('/signin');
-    }, 5000);
+      setLogo(loader_1);
+      setTimeout(() => {
+        setLogo(loader_2)
+        setTimeout(() => {
+          setLogo(loader_3)
+          setTimeout(() => {
+            history.push('/signin');
+          }, 1500);
+        }, 1500);
+      }, 1500);
+    }, 1500);
+
+
   }, []);
+
   return (
     <Container>
-      <Loader src={loader} alt="loader" />
+      <Loader src={logo} alt="loader" />
     </Container>
   );
 };
