640c0b213228b32029b70158bcf38a34a7bf5b32
luisfvsanchez <luisfvsanchez@hotmail.com>
2020-09-26 03:11:30 -0300

    sign in component

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,65 @@
+// Tools
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+// Assets
+import WitcherLogo from '../../assets/images/logo_dark.png';
+
+// Components
+import CustomInput from '../custom-input/custom-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+
+// Actions
+import { logIn } from '../../redux/user/user.actions';
+
+// Styled Components
+import {
+  FormContainer,
+  MainContainer,
+}
+  from './sign-in.stlyes';
+
+const SignIn = ({ logIn }) => {
+  const [userInfo, setUserInfo] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const { email, password } = userInfo;
+    logIn({ email, password });
+  };
+
+  const handleChange = (event) => {
+    const { type, value } = event.target;
+    setUserInfo({ ...userInfo, [type]: value });
+  };
+
+  return (
+    <MainContainer>
+      <img src={WitcherLogo} alt="Witcher Light Logo" />
+      <FormContainer onSubmit={handleSubmit}>
+        <CustomInput change={handleChange} id="userEmail" type="email" placeholder="Email" />
+        <CustomInput change={handleChange} id="userPassword" type="password" placeholder="Password" />
+        <CustomButton isSignIn type="submit">LOGIN</CustomButton>
+      </FormContainer>
+    </MainContainer>
+
+  );
+};
+
+SignIn.defaultProps = {
+  logIn: null,
+};
+
+SignIn.propTypes = {
+  logIn: PropTypes.func,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  logIn: (emailAndPassword) => dispatch(logIn(emailAndPassword)),
+});
+
+export default connect(null, mapDispatchToProps)(SignIn);
---
+++ src/components/sign-in/sign-in.stlyes.jsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  width: 100%;
+  background: rgba(0, 0, 0, 0.7);
+  height: 100%;
+  img {width: 20%};
+
+`;
+export const FormContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  >div:not(:first-child) {
+    margin-bottom: 50px;
+  }
+
+`;
